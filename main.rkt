60262b42a5f960b4e815dcdfbe2f4af5307d133a
helper08 <ccpg02@gmail.com>
2022-02-16 18:19:08 -0500

    getFromState

--- main.rkt
+++ main.rkt
@@ -2,21 +2,34 @@
 
 (require (file "simpleParser.rkt"))
 
-(define M_state
-  (lambda (expr state)))
+;(define M_state
+;  (lambda (expr state)))
+; example state:  (define state '((bob steve buh brunch) (1 2 3 4)))
 
 (define M_value
   (lambda (expr state)
     (cond
       ((null? expr) 0)
       ((number? expr) expr)
-      ((contains? expr state) ) ;check if variable is contained in the state and substitute with its value
-      ((eq? (car expr) '%) (remainder (M_value (cadr expr) state) (M_value (caddr expr) state))))
+      ;((contains? expr state) (getFromState ) ;check if variable is contained in the state and substitute with its value
+      ((not (list? expr)) (getFromState expr state)) 
+      ((eq? (car expr) '%) (remainder (M_value (cadr expr) state) (M_value (caddr expr) state)))
       ((eq? (car expr) '*) (* (M_value (cadr expr) state) (M_value (caddr expr) state)))
       ((eq? (car expr) '/) (/ (M_value (cadr expr) state) (M_value (caddr expr) state)))
       ((eq? (car expr) '+) (+ (M_value (cadr expr) state) (M_value (caddr expr) state)))
       ((eq? (car expr) '-) (- (M_value (cadr expr) state) (M_value (caddr expr) state)))
-      (error "Invalid value expression")))
+      (else (error "Invalid operator")))))
+
+(define getFromState
+  (lambda (var state)
+    (cond
+      ((eq? var (caar state))
+       (if (eq? (cadr state) 'z) (error "Value not declared") (caadr state)))
+      ((null? (cdar state)) (error "Value not initialized"))
+      (else (getFromState var (list (cdar state) (cdadr state)))))))
 
 (define M_boolean
-  (lambda (expr state)))
+  (lambda (expr state)
+    (cond
+      ((null? expr) 
+    ))))
