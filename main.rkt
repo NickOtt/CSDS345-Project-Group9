1a69070b70b28519cd9ed1e36bc94f5fc6aa12fd
helper08 <ccpg02@gmail.com>
2022-02-17 19:55:54 -0500

    Finished stuff

--- main.rkt
+++ main.rkt
@@ -55,17 +55,21 @@
   (lambda (parsetree state)
     (cond
       ((not (list? state)) state)
-      ((state? (caar parsetree)) (interpret-helper ((cdr parsetree) (M_state (car parsetree) state))))
+      ((state? (car parsetree)) (interpret-helper (cdr parsetree) (M_state (car parsetree) state)))
       (else (error "Invalid parse tree")))))
 
 ; (define state '((bob steve buh brunch) (1 2 3 4)))
+
+(define vardefine
+  (lambda (expr state)
+    (if (null? (cddr expr)) 'z (M_value (rightop expr) state))))
 
 (define M_state
   (lambda (expr state)
     (cond
       ((eq? (operator expr) '=) (list (car state) (getUpdatedValues (leftop expr) (M_value (rightop expr) state) state)))
-      ((eq? (operator expr) 'var) (list (append (car state) (cons (leftop expr) '())) (append (car (cdr state)) (cons 'z '()))))
-      ((eq? (operator expr) 'if) (if (M_boolean (leftop expr) state) (M_state (rightop expr) state) (M_state (rightop expr) state)))
+      ((eq? (operator expr) 'var) (list (append (car state) (cons (leftop expr) '())) (append (car (cdr state)) (cons (vardefine expr state) '()))))
+      ((eq? (operator expr) 'if) (if (M_boolean (leftop expr) state) (M_state (rightop expr) state) (M_state (rightrightop expr) state)))
       ((eq? (operator expr) 'while) (if (M_boolean (leftop expr) state) (M_state expr (M_state (rightop expr) state)) (M_state (rightrightop expr) state)))
       ((eq? (operator expr) 'return) (M_value (leftop expr) state))
       (else (error "Invalid state expression")
@@ -81,7 +85,7 @@
 (define M_value
   (lambda (expr state)
     (cond
-      ((null? expr) 0)
+      ((null? expr) 'z)
       ((number? expr) expr)
       ((booleanVal? expr) (booleanVal expr))
       ;((contains? expr state) (getFromState ) ;check if variable is contained in the state and substitute with its value
@@ -89,7 +93,7 @@
       ((eq? (operator expr) '+) (+ (M_value (leftop expr) state) (M_value (rightop expr) state)))
       ((eq? (operator expr) '-) (- (M_value (leftop expr) state) (M_value (rightop expr) state)))
       ((eq? (operator expr) '*) (* (M_value (leftop expr) state) (M_value (rightop expr) state)))
-      ((eq? (operator expr) '/) (/ (M_value (leftop expr) state) (M_value (rightop expr) state)))
+      ((eq? (operator expr) '/) (quotient (M_value (leftop expr) state) (M_value (rightop expr) state)))
       ((eq? (operator expr) '%) (remainder (M_value (leftop expr) state) (M_value (rightop expr) state)))
       ((booleanOp? (operator expr)) (M_boolean expr state))
       (else error "Invalid value expression"))))
